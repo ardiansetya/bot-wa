0c9ccb742acf0733099c5b4cccbce62df313fc38
ardiansetya <ardiansetya129@gmail.com>
2025-05-09 20:57:22 +0700

    feat: login wa & bales message

--- index.js
+++ index.js
@@ -5,7 +5,31 @@ const qrcode = require('qrcode-terminal')
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const client = new Client({
+    authStrategy: new LocalAuth({ clientId: 'andriana' })
+})
+
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true })
+})
+
+client.on("authenticated", () => {
+    console.log("udah login!")
+})
+
+client.on("ready", () => {
+    console.log("ready gan!")
+})
+
+client.on("message", message => {
+    if (message.body === "woi asu") {
+        message.reply("apa tohapokkkkk")
+    }
+})
+
+client.initialize()
 
 
 app.listen(port, () => {
